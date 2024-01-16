df3a7eb571844a7a07c5aab6a332365f38f4c13c
stan@Inspirion <stanislaw.sochacki@outlook.com>
2024-01-16 11:42:18 +0100

    Full CRUD products

--- db_api.js
+++ db_api.js
@@ -25,26 +25,25 @@ async function get_users(){
 async function new_user(name,dob,mail,address){
     try{
         var result = await pool.query('INSERT INTO users (name,dob,email,address) \
-                                            values ($1,$2,$3,$4) RETURNING id',
+                                            VALUES ($1,$2,$3,$4) RETURNING id',
                         [name,dob,mail,address]);
         return result.rows[0].id;
     }
     catch (error){
-        console.error('Error crating new user:', error);
+        console.error('Error creating new user:', error);
         throw error;
     }
 }
 // edit data of a user with given id
 async function edit_user(id,name,dob,mail,address){
     try{
-        var result = await pool.query('UPDATE users \
-                                        SET name=$1, \
-                                        SET dob=$2, \
-                                        SET email=$3, \
-                                        SET address=$4 \
-                                        WHERE users.id=$5',
-                                    [name,dob,mail,address,id]);
-        return result;
+        await pool.query('UPDATE users \
+                            SET name=$1, \
+                            SET dob=$2, \
+                            SET email=$3, \
+                            SET address=$4 \
+                            WHERE users.id=$5',
+                        [name,dob,mail,address,id]);
     }
     catch (error){
         console.error("Error updating user's data:", error);
@@ -54,10 +53,9 @@ async function edit_user(id,name,dob,mail,address){
 // remove user of given id
 async function delete_user(id){
     try{
-        const result = await pool.query('DELETE FROM users \
-                                        WHERE users.id=$1',
-                                        [id]);
-        return result;
+        await pool.query('DELETE FROM users \
+                            WHERE users.id=$1',
+                            [id]);
     }
     catch (error){
         console.error('Error removing use from database:', error);
@@ -65,9 +63,78 @@ async function delete_user(id){
     }
 }
 //----PRODUCTS----------------------------
+// show all products in database
 async function get_products(){
-    const {rows} = await pool.query('SELECT * FROM products');
-    return rows;
+    try{
+        const {rows} = await pool.query('SELECT * FROM products');
+        return rows;
+    }
+    catch(error){
+        console.error('Error showing products:',error);
+        throw error;
+    }
+}
+// show product of given id
+async function get_product(id){
+    try{
+        const {rows} = await pool.query('SELECT * FROM products \
+                                        WHERE products.id=$1',
+                                        [id]);
+        return rows[0];
+    }
+    catch (error){
+        console.error('Error showing product',error);
+        throw error;
+    }
+}
+// create new product 
+async function new_product(name,quantity,price,category,colour,height,width,depth,style,material,image){
+    try{
+        await pool.query('INSERT INTO products \
+                            (name,quanitty,price,category,colour,height,width,depth,style,material,image) \
+                            VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)',
+                        [name,quantity,price,category,colour,height,width,depth,style,material,image]);
+    }
+    catch (error){
+        console.error('Error creating new product:', error);
+        throw error;
+    }
+}
+// edit data of a product with given id
+async function edit_product(id,name,quantity,price,category,colour,height,width,depth,style,material,image){
+    try{
+        await pool.query('UPDATE products \
+                            SET name=$1, \
+                            SET quantity=$2, \
+                            SET price=$3, \
+                            SET category=$4, \
+                            SET colour=$5, \
+                            SET height=$6, \
+                            SET wodth=$7, \
+                            SET depth=$8, \
+                            SET style=$9, \
+                            SET material=$10, \
+                            SET image=$11, \
+                            WHERE products.id=$12',
+                        [name,quantity,price,category,colour,height,width,depth,style,material,image,id]);
+    }
+    catch (error){
+        console.error("Error updating product's data:", error);
+        throw error;
+    }
+}
+// remove product of given id
+async function delete_product(id){
+    try{
+        const result = await pool.query('DELETE FROM products \
+                                        WHERE products.id=$1',
+                                        [id]);
+        return result;
+    }
+    catch (error){
+        console.error('Error removing product from database:', error);
+        throw error;
+    }
 }
 
 //----LOGINS----------------------------
@@ -103,12 +170,13 @@ async function get_logins(){
 // create new login
 async function new_login(id,usr,pwd){
     try{
-        await pool.query('INSERT INTO logins (user_id,login,password,role) \
-                                        values ($1,$2,$3,$4)',
+        await pool.query('INSERT INTO \
+                            logins (user_id,login,password,role) \
+                            VALUES ($1,$2,$3,$4)',
                         [id,usr,pwd,'user']);
     }
     catch (error){
-        console.error('Error crating new login:', error);
+        console.error('Error creating new login:', error);
         throw error;
     }
 }
